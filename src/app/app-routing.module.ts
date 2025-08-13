26a8991b0434f2c15333c1f2c8e8344cd8248200
PardiniO <pardinioriana@gmail.com>
2025-08-13 18:31:30 -0300

    First routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "../app/pages/home/home.component";
+import { ExploreComponent } from "../app/pages/explore/explore.component";
+import { LibraryComponent } from "../app/pages/library/library.component";
+import { ReaderComponent } from "../app/pages/reader/reader.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'explore', component: ExploreComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: 'reader/:id', component: ReaderComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
