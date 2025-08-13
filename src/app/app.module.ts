e8079be939b6509358fad33c6261e692f67b6bcb
PardiniO <pardinioriana@gmail.com>
2025-08-13 20:11:20 -0300

    Add http provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,9 @@ import { ProfileComponent } from './pages/profile/profile.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  login(username: string, password: string) {
+    // Implement login logic
+  }
+
+  logout() {
+    // Implement logout logic
+  }
 }
