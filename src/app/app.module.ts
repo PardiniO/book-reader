0eb2784b2c717355bf4e053cbeddb3baa02cba8a
PardiniO <pardinioriana@gmail.com>
2025-08-13 18:13:31 -0300

    Creation of main pages of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LibraryComponent } from './pages/library/library.component';
+import { ReaderComponent } from './pages/reader/reader.component';
+import { ExploreComponent } from './pages/explore/explore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchBarComponent,
+    HomeComponent,
+    LibraryComponent,
+    ReaderComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
